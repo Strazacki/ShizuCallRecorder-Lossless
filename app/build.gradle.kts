59dd68028e8a68f1a9664ed5dde671cdac490230
Sebastian Cabaj <TWJ_EMAIL>
2026-08-23 22:56:41 +0200

    Bump version for lossless fork

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,8 +133,8 @@ android {
         minSdk = 30
         targetSdk = 36
         // Keep theses two values hard-coded here and update them per version. (To keep F-Droid compatibility since their parser is very basic)
-        versionCode = 15
-        versionName = "1.2.0"
+        versionCode = 20
+        versionName = "1.3.3-lossless"
 
         buildConfigField("String", "SCRCPY_VERSION", "\"$scrcpyVersion\"")
         buildConfigField("String", "SCRCPY_SERVER_SHA256", "\"$scrcpyServerSha256\"")
